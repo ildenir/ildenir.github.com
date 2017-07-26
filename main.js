19bed5475e3b06d917a2dfe25df4ec9cc9a0dac6
Ildenir Barbosa <ildenir+github@googlemail.com>
2017-07-25 22:19:26 -0300

    Esqueleto da pagina principal com menu principal.

---
+++ main.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("conteudo").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("conteudo").style.margin = "0";
+}
