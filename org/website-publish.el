876f57a63807eb9904b0e4bd84cfe0875c0861c2
Ildenir Barbosa <ildenir+github@googlegmail.com>
2017-10-06 22:26:45 -0300

    Ignora arquivos org iniciados com espaco e hifen

--- org/website-publish.el
+++ org/website-publish.el
@@ -97,7 +97,7 @@ exista. Description vai ser extraida de #+DESCRIPTION:"
          :auto-preamble t
          :org-html-doctype html5
          :org-html-html5-fancy t
-         :exclude "^\-.+"
+         :exclude "^ *\-.+"
          :html-preamble ,website-html-preamble
          :html-postamble-format ""
          :html-head ,website-html-head
